6e97767ee1754d125e9a144ee6c82ae83526e63f
ravindrapallikonda <ravindrapallikonda@gmail.com>
2024-08-19 22:01:22 +0200

    refactor(fetchdata): refactored usequery and fetch

--- src/hooks/usePackage.ts
+++ src/hooks/usePackage.ts
@@ -1,41 +1,53 @@
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
-import { API_KEY, API_URL } from "@/consts";
+import { API_URL } from "@/consts";
 
-import { Packages } from "@/lib/types";
+import { SearchResults } from "@/lib/interface";
 
 /**
  * Fetch packages based on the search term.
- * @param searchTerm - The term to search for movies.
+ * @param searchTerm - The term to search for packages.
+ * @param page - page number, to fetch the records for that page.
+ * @param perPage - Number of records to show per page.
  * @returns A promise that resolves to a list of packages or throws an error.
  */
 export const fetchPackages = async (
   searchTerm: string, page: number, perPage: number
-): Promise<Packages | null> => {
+): Promise<SearchResults> => {
   try {
     // Encoding the search term correctly
     const encodedSearchTerm = encodeURIComponent(searchTerm);
-    const url = `${API_URL}/?q=${encodedSearchTerm}?&api_key=${API_KEY}&page=${page}&per_page=${perPage}`;
+    
+    // getting api key env file. This is from .env.devlopment file
+    const apiKey = import.meta.env.VITE_API_KEY;
 
+    const url = `${API_URL}/?q=${encodedSearchTerm}&api_key=${apiKey}&page=${page}&per_page=${perPage}`;
+    
     const response = await fetch(url);
+
+    // taking total records from response header
+    // const records = Object.fromEntries(response.headers.entries()).total;
+    const records = response.headers.get("total") || 5;
 
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
+    // packages data as json
     const data = await response.json() ;
 
-    return data as Packages;
+    return {
+      searchData: data,
+      totalRecords: Number(records)
+    };
   } catch (error) {
-    console.error("An unexpected error occurred:", error);
-    return null;
+    throw new Error(`Error while fetching packages ${error}`);
   }
 };
 
 export const usePackageSearch = (searchTerm: string, page: number, perPage: number) => {
-  return useQuery({
-    queryKey: ["packages", page],
+  return useQuery<SearchResults>({
+    queryKey: ["packages", searchTerm, page, perPage],  // include perPage in the query key
     queryFn: () => fetchPackages(searchTerm, page, perPage),
-    enabled: !!searchTerm,
-    placeholderData: keepPreviousData
+    placeholderData: keepPreviousData,
   });
 };
